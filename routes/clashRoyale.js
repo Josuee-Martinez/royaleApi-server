3a2d13c70a50f3b432d42b2f306f0701cb4d965f
Josue Martinez <josue@Josues-MacBook-Pro.local>
2019-12-20 23:22:17 -0500

    player stats

--- routes/clashRoyale.js
+++ routes/clashRoyale.js
@@ -11,9 +11,28 @@ router.get("/", async (req, res) => {
     };
     const axiosRes = await axios.get(process.env.apiUrl, config);
 
-    res.json({ data: axiosRes.data });
+    res.json(axiosRes.data);
   } catch (error) {
-    console.error(err.message);
+    console.error(error.message);
+    res.status(500).send("server error");
+  }
+});
+
+router.get("/:tag", async (req, res) => {
+  try {
+    const config = {
+      headers: {
+        auth: process.env.apiKey
+      }
+    };
+    const axiosRes = await axios.get(
+      `https://api.royaleapi.com/player/${req.params.tag}`,
+      config
+    );
+
+    res.json(axiosRes.data);
+  } catch (error) {
+    console.error(error.message);
     res.status(500).send("server error");
   }
 });
