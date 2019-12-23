8fa917e5810fe241e3d2e7ec843cf8a35ece7573
Josue Martinez <josue@Josues-MacBook-Pro.local>
2019-12-23 00:08:42 -0500

    chest data

--- routes/clashRoyale.js
+++ routes/clashRoyale.js
@@ -37,4 +37,23 @@ router.get("/:tag", async (req, res) => {
   }
 });
 
+router.get("/:tag/chests", async (req, res) => {
+  try {
+    const config = {
+      headers: {
+        auth: process.env.apiKey
+      }
+    };
+    const axiosRes = await axios.get(
+      `https://api.royaleapi.com/player/${req.params.tag}/chests`,
+      config
+    );
+
+    res.json(axiosRes.data);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("server error");
+  }
+});
+
 module.exports = router;
